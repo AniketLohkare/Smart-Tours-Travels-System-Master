734be313e8a810ef3f0027c43a97ce3fe07638c6
AniketLohkare <87561202+AniketLohkare@users.noreply.github.com>
2025-05-13 17:08:35 +0530

    project

---
+++ common/footer.php
@@ -0,0 +1,77 @@
+<!-- FOOTER SECTION STARTS -->
+					
+				<div class="footer col-sm-12">
+					
+					<div class="col-sm-4">
+						
+						<div class="footerHeading">
+							Contact Us
+						</div>
+							
+						<div class="footerText">
+							Tanuja Agencies <br>  Kankavli-416602, Sindhudurga, India
+						</div>
+				
+						<div class="footerText">
+							E-mail: queries@travelwale.com
+						</div>
+						
+					</div>
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Made with
+						</div>
+						
+						<div class="fa fa-heart"></div>
+						
+						<div class="footerHeading">
+							in India
+						</div>
+						
+						<div class="flagContainer text-center">
+							<img src="images/flag.png">
+						</div>
+						
+					</div>
+					
+					<div class="col-sm-4">
+					
+						<div class="footerHeading">
+							Social Links
+						</div>
+						
+						<div class="socialLinks">
+						
+							<div class="fb">
+								facebook.com/travelwale
+							</div>
+						
+							<div class="gp">
+								plus.google.com/travelwale
+							</div>
+						
+							<div class="tw">
+								twitter.com/travelwale
+							</div>
+						
+							<div class="in">
+								linkedin.com/travelwale
+							</div>
+						
+						</div> <!-- social links -->
+						
+					</div>
+						
+					<div class="col-sm-12">
+					<div class="copyrightContainer">
+						<div class="copyright">
+						Online Tours And Travels &copy; Tanuja And Group.
+						</div>
+					</div>
+					</div>
+							
+				</div> <!-- footer -->
+				
+			<!-- FOOTER SECTION ENDS -->
